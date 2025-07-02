8664886273bc3667fd7a5aefe457b3d449b97326
carlos5veiga <carlos5veiga@gmail.com>
2025-07-02 09:27:02 -0300

    edit

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -1,12 +1,13 @@
-import {Component, effect, inject, signal} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
-import {Course} from "../models/course.model";
-import {EditCourseDialogData} from "./edit-course-dialog.data.model";
-import {CoursesService} from "../services/courses.service";
-import {LoadingIndicatorComponent} from "../loading/loading.component";
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
-import {CourseCategoryComboboxComponent} from "../course-category-combobox/course-category-combobox.component";
-import {CourseCategory} from "../models/course-category.model";
+import { Component, effect, inject, signal } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogRef } from "@angular/material/dialog";
+import { Course } from "../models/course.model";
+import { EditCourseDialogData } from "./edit-course-dialog.data.model";
+import { CoursesService } from "../services/courses.service";
+import { LoadingIndicatorComponent } from "../loading/loading.component";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { CourseCategoryComboboxComponent } from "../course-category-combobox/course-category-combobox.component";
+import { CourseCategory } from "../models/course-category.model";
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'edit-course-dialog',
@@ -21,5 +22,20 @@ import {CourseCategory} from "../models/course-category.model";
 })
 export class EditCourseDialogComponent {
 
+  dialogRef = inject(MatDialogRef);
+  onClose() {
+    this.dialogRef.close();
+  }
 
+}
+
+export async function openEditCourseDialog(dialog: MatDialog, data: EditCourseDialogData) {
+  const config = new MatDialogConfig();
+  config.disableClose = true;
+  config.autoFocus = true;
+  config.width = "400px";
+  config.data = data;
+  const close$ = dialog.open(EditCourseDialogComponent, config)
+    .afterClosed();
+  return firstValueFrom(close$);
 }
